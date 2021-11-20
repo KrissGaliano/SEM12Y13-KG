6aabfd8a00509d2cf36c6e94d89e4f13911a1bcd
KrissGaliano <kriss.galiano@tecup.edu.pe>
2021-11-19 21:34:38 -0500

    semana 12 y 13

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'kriss-version.cpfncagmdjlm.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: '12345678',
+        database: 'agenda_KG'
+    }
+
+};
